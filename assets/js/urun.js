84b533aee69bb42458b427704985a12a3ea93d01
Berk Uçar <135522301+BarrickUAR@users.noreply.github.com>
2024-06-28 16:10:36 +0300

    Update urun.js

--- assets/js/urun.js
+++ assets/js/urun.js
@@ -1,7 +1,7 @@
 let form = document.querySelector('.catchForm');
 let productListHere = document.querySelector('.productListHere');
 let clearButton = document.querySelector('.clear');
-let productList = [];
+let productList = JSON.parse(localStorage.getItem('productList')) || [];
 
 function handleFormSubmit(e) {
   e.preventDefault();
